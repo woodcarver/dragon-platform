f475c8fb9ea5efe06745ca93fd69cef573d2962a
woodcarver <woodcarvercello@gmail.com>
2018-09-12 00:12:19 +0800

    feature: add detail api

--- dragon_server/dragon_app/api/simulations/serializers.py
+++ dragon_server/dragon_app/api/simulations/serializers.py
@@ -1,7 +1,10 @@
-from flask_restplus import fields
+from flask_restplus import fields, marshal
 from dragon_app.api.restplus import api
 
 simulation = api.model("simulation",{
         'id': fields.Integer(readOnly=True, description='The unique identifier of a blog simulation'),
         'title': fields.String(required=True, description='Simulation title'),
         })
+
+def format_response(marshal_model, data):
+    return {'code':0, 'data':marshal(data, marshal_model)}
--- dragon_server/dragon_app/api/simulations/simulations.py
+++ dragon_server/dragon_app/api/simulations/simulations.py
@@ -1,10 +1,11 @@
 import logging
+import json
 
 from flask import request
 from flask_restplus import Resource
 from dragon_app.api.restplus import api
 from dragon_app.database.models import Simulation
-from dragon_app.api.simulations.serializers import simulation
+from dragon_app.api.simulations.serializers import format_response, simulation
 
 log = logging.getLogger(__name__)
 
@@ -12,11 +13,20 @@ ns = api.namespace('simulations', description='Operations related to simulation'
 
 @ns.route('/list/latest')
 class SimulationCollection(Resource):
-    @api.marshal_list_with(simulation)
+    #@api.marshal_list_with(simulation)
     def get(self):
         """
         Returns latest list of simulations.
         """
-        #simulations = [1,2,3]
-        simulations = Simulation.query.all()
-        return simulations
+        datalist = Simulation.query.all()
+        return format_response(simulation, datalist)
+
+
+@ns.route('/detail/<int:id>')
+class SimulationCollection(Resource):
+    def get(self, id):
+        """
+        Returns detail of one simulation.
+        """
+        data = Simulation.query.filter(Simulation.id == id).one()
+        return format_response(simulation, data)
